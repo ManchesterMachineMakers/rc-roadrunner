e564217edf07a5c52f2a33b23ef3a32f2e2e1fc5
milesavernon <milesavernon@gmail.com>
2024-12-28 16:50:53 -0500

    organized opmodes
    Signed-off-by: milesavernon <milesavernon@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/TankDrive.java
@@ -61,7 +61,7 @@ import java.util.List;
 public final class TankDrive {
     public static class Params {
         // IMU orientation
-        // TODO: fill in these values based on
+        // fill in these values based on
         //   see https://ftc-docs.firstinspires.org/en/latest/programming_resources/imu/imu.html?highlight=imu#physical-hub-mounting
         public RevHubOrientationOnRobot.LogoFacingDirection logoFacingDirection =
                 RevHubOrientationOnRobot.LogoFacingDirection.UP;
